bb579e37ec4aa7c5232707715aae5d82b49f2592
Tre Lannon <lannon@usc.edu>
2018-11-02 21:54:48 -0700

    Update sql/foodbook_db.sql
    Foodbook Database 1

---
+++ sql/foodbook_db.sql
@@ -0,0 +1,26 @@
+DROP DATABASE if exists foodbook;
+CREATE DATABASE foodbook;
+USE foodbook;
+
+CREATE TABLE Users (
+  userID int AUTO_INCREMENT NOT NULL UNIQUE,
+  email varchar(50) PRIMARY KEY NOT NULL UNIQUE,
+  name varchar(40) NOT NULL,
+  imgURL varchar(200),
+  bio varchar(140),
+  contactinfo varchar(50) NOT NULL
+);
+
+CREATE TABLE Offerings (
+  offeringID int AUTO_INCREMENT PRIMARY KEY NOT NULL UNIQUE,
+  chefEmail varchar(50) NOT NULL,
+  FOREIGN KEY fk1(chefEmail) REFERENCES Users(email),
+  cuisineType varchar(20) NOT NULL,
+  name varchar(80) NOT NULL,
+  description varchar(280) NOT NULL,
+  price double NOT NULL,
+  location varchar(120) NOT NULL,
+  startTime bigint NOT NULL,
+  endTime bigint NOT NULL,
+  rating int(1) NOT NULL
+);
