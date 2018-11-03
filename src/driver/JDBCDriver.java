cde8df3a90c9c6b60bd1ba55ead8a57a4a4e8ce3
AlexColello <acolello@usc.edu>
2018-11-02 22:23:48 -0700

    Added open and close to JDBCDriver

--- src/driver/JDBCDriver.java
+++ src/driver/JDBCDriver.java
@@ -1,7 +1,47 @@
-
 package driver;
+import java.sql.*;
 
+import datatypes.User;
 
 public class JDBCDriver {
-
+	private static Connection conn = null;
+	private static ResultSet rs = null;
+	private static PreparedStatement ps = null;
+	
+	
+	public static boolean connect(){
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost/foodbook_db?user=root&password=root&useSSL=false");
+			return true;
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	public static void close(){
+		try{
+			if (rs!=null){
+				rs.close();
+				rs = null;
+			}
+			if(conn != null){
+				conn.close();
+				conn = null;
+			}
+			if(ps != null ){
+				ps = null;
+			}
+		}catch(SQLException sqle){
+			System.out.println("connection close error");
+			sqle.printStackTrace();
+		}
+	}
+	
 }
