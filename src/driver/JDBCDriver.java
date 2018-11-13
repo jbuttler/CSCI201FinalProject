88fceb2898a80b2ebba0d25a22d707675c975a65
AlexColello <acolello@usc.edu>
2018-11-12 23:18:53 -0800

    Added tests for the search functionality.

--- src/driver/JDBCDriver.java
+++ src/driver/JDBCDriver.java
@@ -116,6 +116,7 @@ public class JDBCDriver {
 	
 	public static ArrayList<User> findUsers(String query) {
 		ArrayList<User> users = new ArrayList<User>();
+		
 		if(query.equals("")) {
 			users = getAllUsers();
 		}
